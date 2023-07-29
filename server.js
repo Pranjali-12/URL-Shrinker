da82f3f6efef8a3e736ef31d09e7b1b5a7d7cf38
Pranjgit <kasbalepranjali@gmail.com>
2023-07-29 16:36:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const mongoose=require('mongoose')
+const env=require('dotenv');
+
+const shortUrl=require('./models/shorturl');
+
+env.config();
+
+const app=express();
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser:true,useUnifiedTopology:true
+})
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}))
+
+app.get('/',async (req,res)=>{
+    const shortUrls=await shortUrl.find();
+    res.render('index',{shortUrls:shortUrls});
+})
+
+app.post('/shorturls',async (req,res)=>{
+    await shortUrl.create({full:req.body.fullUrl})
+    res.redirect('/');
+})
+
+app.get('/:shortUrl',async (req,res)=>{
+    const ShortUrl= await shortUrl.findOne({short:req.params.shortUrl});
+    if(ShortUrl==null){
+        return res.sendStatus(404);
+    }
+
+    ShortUrl.clicks++;
+    ShortUrl.save()
+
+    res.redirect(ShortUrl.full)
+})
+
+app.listen(process.env.PORT)
